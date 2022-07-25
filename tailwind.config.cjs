0b1fc41c31ca1ef8bc000eb2277db8efd467a561
Richson Simbabure <richsonsimbabure@gmail.com>
2022-07-25 23:06:51 +0200

    created components🛴

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,16 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				body: ['Roboto', 'sans-serif']
+			},
+			colors:{
+				accent: '#8080D7',
+				bright: '#F1E6E6',
+				bodyBg: '#010922'
+			}
+		},
 	},
 	plugins: [],
 }
